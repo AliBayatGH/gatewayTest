be035d357779b3cebd440edfc23eb91dfc4eac8b
Ali Bayat <Bayaat.Ali@Hasti.co>
2023-01-09 17:50:21 +0330

    Add project files.

---
+++ src/WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public async Task<IEnumerable<string>> Get()
+        {
+            HttpClient httpClient = new HttpClient();
+            var response1 = await httpClient.GetAsync("http://localhost:5002/api/Values/1");
+            var response2 = await httpClient.GetAsync("http://localhost:5003/api/Values/2");
+
+            var result1 = await response1.Content.ReadAsStringAsync();
+            var result2 = await response2.Content.ReadAsStringAsync();
+            var result = result1 + result2;
+
+            return new string[] { result, "value2" };
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ tests/WebApplication1Tests/Controllers/ValuesControllerTests.cs
@@ -0,0 +1,56 @@
+﻿using Xunit;
+using WebApplication1.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+using System.ComponentModel;
+
+namespace WebApplication1.Controllers.Tests
+{
+    public class ValuesControllerTests
+    {
+        [Fact()]
+        public async Task GetTest()
+        {
+            //var webApplicationFactory = new WebApplicationFactory<Program>();
+            //var client = webApplicationFactory.CreateClient();
+            //var result = await client.GetAsync("http://localhost/api/Values/");
+
+            // Start the service 1 container
+            ITestcontainersContainer container1 = new TestcontainersBuilder<TestcontainersContainer>()
+                .WithImage("service2:dev")
+                .WithPortBinding(80, true)
+                .WithExposedPort(80)
+                .WithPortBinding(5002, 80)
+                .Build();
+
+            await container1.StartAsync();
+
+            var _service1Container = new TestcontainersBuilder<TestcontainersContainer>()
+                    .WithImage("service2:dev")
+                    .WithPortBinding(80, true)
+                    .WithExposedPort(80)
+                    .WithPortBinding(5002, 80)
+                    .Build();
+            await _service1Container.StartAsync();
+
+            // Start the service 2 container
+            TestcontainersContainer _service2Container = new TestcontainersBuilder<TestcontainersContainer>()
+                    .WithImage("service3:dev")
+                    .WithPortBinding(80, true)
+                    .WithPortBinding(80)
+                    .WithExposedPort(5003)
+                    .Build();
+            _service2Container.StartAsync().GetAwaiter().GetResult();
+
+            var webApplicationFactory = new WebApplicationFactory<Program>();
+            var client = webApplicationFactory.CreateClient();
+            var result = await client.GetAsync("http://localhost/api/Values/");
+        }
+    }
+}
